612f0af62b5fd7ebf21e31a0f8607dd68981ae22
RithmaRandima <rithma.r.nandasiri.com>
2025-06-18 08:49:57 +0530

    Completed Header section

---
+++ src/assets/Data.js
@@ -0,0 +1,74 @@
+export const data = [
+  {
+    id: 1,
+    name: "Double CheeseBurger",
+    image: "./Double-Cheeseburger.webp",
+    price: "$$$$",
+    category: "burger",
+  },
+  {
+    id: 2,
+    name: "Loaded Burger",
+    image: "./loaded-burgers.jpg",
+    price: "$$$$",
+    category: "burger",
+  },
+  {
+    id: 3,
+    name: "Burger",
+    image: "./burger.jpg",
+    price: "$$$$",
+    category: "burger",
+  },
+  {
+    id: 4,
+    name: "Mushroom Burger",
+    image: "./Mushroom-Burger.jpg",
+    price: "$$$$",
+    category: "burger",
+  },
+  {
+    id: 5,
+    name: "Seafood Pizza",
+    image: "./seafood-Pizza.jpg",
+    price: "$$$$",
+    category: "pizza",
+  },
+  {
+    id: 6,
+    name: "Spinach Pizza",
+    image: "./spinach-pizza.webp",
+    price: "$$$$",
+    category: "pizza",
+  },
+  {
+    id: 7,
+    name: "Supreme Pizza",
+    image: "./supreme-pizza.avif",
+    price: "$$$$",
+    category: "pizza",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    name: "Fast Food",
+    image: "./Double-Cheeseburger.webp",
+  },
+  {
+    id: 2,
+    name: "Pizza",
+    image: "./Double-Cheeseburger.webp",
+  },
+  {
+    id: 3,
+    name: "Wings",
+    image: "./Double-Cheeseburger.webp",
+  },
+  {
+    id: 4,
+    name: "Salad",
+    image: "./Double-Cheeseburger.webp",
+  },
+];
